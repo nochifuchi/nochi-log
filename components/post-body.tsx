f8138a78044b285cda4ef3f54dac418626218a2e
Fumitaka Nozawa <nozawa@bmc-net.jp>
2023-09-11 09:02:39 +0900

    マークダウンをzenn-markdownに変更

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,4 +1,4 @@
-import markdownStyles from './markdown-styles.module.css'
+import 'zenn-content-css';
 
 type Props = {
   content: string
@@ -6,9 +6,8 @@ type Props = {
 
 const PostBody = ({ content }: Props) => {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-2xl mx-auto znc">
       <div
-        className={markdownStyles['markdown']}
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
