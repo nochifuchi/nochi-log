3b00a182654fe622c2cca250848c95b0b2f61a1d
Fumitaka Nozawa <nozawa@bmc-net.jp>
2023-10-30 09:19:17 +0900

    ヘッダー不要クラス削除

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="flex items-center h-20 mb-5 mx-2 max-w-screen-lg lg:mx-auto max-w-screen-lg lg:mx-auto w-full">
       <h1>
-        <Link href="/" className="hover:underline">
+        <Link href="/">
           <img src="/assets/common/logo.svg" alt="NOCHI LOG" />
         </Link>
       </h1>
