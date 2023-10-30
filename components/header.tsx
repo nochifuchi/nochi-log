a2ac17246283c10125aef74753756de7df58fc7c
Fumitaka Nozawa <nozawa@bmc-net.jp>
2023-10-31 08:57:11 +0900

    ヘッダーレイアウト調整

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <header className="flex items-center h-20 mb-5 mx-2 max-w-screen-lg lg:mx-auto max-w-screen-lg lg:mx-auto w-full">
+    <header className="flex items-center h-20 mb-5 mx-2 max-w-screen-lg lg:mx-auto max-w-screen-lg lg:mx-auto">
       <h1>
         <Link href="/">
           <img src="/assets/common/logo.svg" alt="NOCHI LOG" />
