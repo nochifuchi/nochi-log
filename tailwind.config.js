ea8763f6144452b7de8f2b2c08f2122c5423a6c4
Fumitaka Nozawa <nozawa@bmc-net.jp>
2023-11-01 08:33:40 +0900

    tailwind.config.jsにカラー設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,30 +10,11 @@ module.exports = {
         ]
       },
       colors: {
-        'accent-1': '#FAFAFA',
-        'accent-2': '#EAEAEA',
-        'accent-7': '#333',
-        success: '#0070f3',
-        cyan: '#79FFE1',
-      },
-      spacing: {
-        28: '7rem',
-      },
-      letterSpacing: {
-        tighter: '-.04em',
-      },
-      lineHeight: {
-        tight: 1.2,
-      },
-      fontSize: {
-        '5xl': '2.5rem',
-        '6xl': '2.75rem',
-        '7xl': '4.5rem',
-        '8xl': '6.25rem',
-      },
-      boxShadow: {
-        sm: '0 5px 10px rgba(0, 0, 0, 0.12)',
-        md: '0 8px 30px rgba(0, 0, 0, 0.12)',
+        'primary': {
+          'black': '#000',
+          'dark-grey': '#d9d9d9',
+          'grey': '#ececec'
+        }
       },
     },
   },
