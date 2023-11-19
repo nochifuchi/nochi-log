b1a0e50ec94daf4aeab3b04ec6ceb01699fe134a
Fumitaka Nozawa <nozawa@bmc-net.jp>
2023-11-20 08:57:20 +0900

    トップページ以外のヘッダーロゴのタグ修正

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,24 @@
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <header className="flex items-center h-20 mb-1 md:mb-5 mx-2 max-w-screen-lg lg:mx-auto max-w-screen-lg lg:mx-auto">
-      <h1>
-        <Link href="/">
-          <img src="/assets/common/logo.svg" width="179" height="48" alt="NOCHI LOG" />
-        </Link>
-      </h1>
+      {router.pathname === '/' ? (
+        <h1>
+          <Link href="/">
+            <img src="/assets/common/logo.svg" width="179" height="48" alt="NOCHI LOG" />
+          </Link>
+        </h1>
+      ) : (
+        <div>
+          <Link href="/">
+            <img src="/assets/common/logo.svg" width="179" height="48" alt="NOCHI LOG" />
+          </Link>
+        </div>
+      )}
     </header>
   )
 }
